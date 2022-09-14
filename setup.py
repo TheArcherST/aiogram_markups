ad164ec13f84a05adf5f06b0164cbbb6222aa85c
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-09-14 18:15:01 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+
+setuptools.setup(name="aiogram_keyboards", version="1.0",
+                 packages=setuptools.find_packages(),
+                 url='#',
+                 author='Shapovalov Mihail',
+                 author_email='mihailsapovalov05@gmail.com',
+                 zip_safe=False)
