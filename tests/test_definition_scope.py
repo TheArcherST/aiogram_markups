8f6bdc7d1b5054d010ccc88a6c8983dafccc08ab
Shapovalov Mihail <67328068+TheArcherST@users.noreply.github.com>
2022-10-28 21:59:33 +0300

    Fix test for DefinitionScope

--- tests/test_definition_scope.py
+++ tests/test_definition_scope.py
@@ -1,11 +1,21 @@
 import pytest
 
+from aiogram import Dispatcher, Bot
 from aiogram.types import Message
+from aiogram_keyboards import setup_aiogram_keyboards
 from aiogram_keyboards.core.button import DefinitionScope
 
 
+@pytest.fixture()
+def dp():
+    bot = Bot('1:faketoken')
+    dispatcher = Dispatcher(bot)
+
+    setup_aiogram_keyboards(dispatcher)
+
+
 @pytest.mark.asyncio
-async def test_commands():
+async def test_commands(dp):
     scope = DefinitionScope(commands=['start', 'ex'])
 
     assert await scope.filter(Message(text='/start'))
@@ -15,7 +25,7 @@ async def test_commands():
 
 
 @pytest.mark.asyncio
-async def test_text():
+async def test_text(dp):
     scope = DefinitionScope(text=['start', 'other text.'])
 
     assert not await scope.filter(Message(text='other text./'))
